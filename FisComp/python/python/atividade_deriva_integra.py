51f4f21c8930eb46f8bfcdf0daf03bdeec0f7550
Augusto Rodrigues <augusto.cr2005@gmail.com>
2025-06-16 09:48:46 -0300

    teste

--- FisComp/python/python/atividade_deriva_integra.py
+++ FisComp/python/python/atividade_deriva_integra.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 A = 2
 B = 0.5
 OMEGA = np.pi
-
+DX = 1e-9
 
 def derivative(f,x, dx):
     return (f(x+dx) - f(x-dx))/(2*dx)
@@ -23,8 +23,8 @@ def analytic_velocity(t):
 def acceleration(f, t, dx):
     return second_derivative(f, t, dx)
 
-def analytic_acceleration():
-    return 1 - 2*(OMEGA**)2*np.sin(OMEGA*t)
+def analytic_acceleration(t):
+    return 1 - 2*(OMEGA**2)*np.sin(OMEGA*t)
 
 
 
@@ -32,14 +32,26 @@ t = np.arange(0, 10.1, 0.1)
 
 
 
-velocities = velocity(f, t, 1e-9)
+velocities = velocity(f, t, DX)
 analytic_velocities = analytic_velocity(t)
 
 
-plt.plot(t, velocities, '*--', color='blue', label='Velocidade Numérica')
-plt.plot(t, analytic_velocities, color='red', label='Velocidade Analitica')
+# plt.plot(t, velocities, '*--', color='blue', label='Velocidade Numérica')
+# plt.plot(t, analytic_velocities, color='red', label='Velocidade Analitica')
+# plt.ylabel('Tempo (s)')
+# plt.xlabel('Velocidade (m/s)')
+# plt.legend()
+# plt.grid()
+# plt.show()
+
+
+acceleretions = acceleration(f, t, DX)
+analytic_accelerations = analytic_acceleration(t)
+
+plt.plot(t, acceleretions, '*--', color='blue', label='Aceleração Numérica')
+plt.plot(t, analytic_accelerations, color='red', label='Aceleração Analitica')
 plt.ylabel('Tempo (s)')
-plt.xlabel('Velocidade (m/s)')
+plt.xlabel('Aceleração (m/s²)')
 plt.legend()
 plt.grid()
 plt.show()
